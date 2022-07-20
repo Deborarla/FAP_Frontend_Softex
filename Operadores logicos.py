396c372db286cef92a694f6a057120764b555d1a
Deborarla <103007838+Deborarla@users.noreply.github.com>
2022-07-20 13:52:38 -0300

    Add files via upload

---
+++ Operadores logicos.py
@@ -0,0 +1,10 @@
+nome=str( input('Digite o nome do aluno: ') )
+nota1=float(input('Digite a primeira nota:'))
+nota2=float(input('Digite a segunda nota:'))
+falta=int(input('Digite o total de faltas:'))
+media=((nota1 + nota2)/2)
+
+if media<7 or falta > 3:
+    print(nome, ',voce está reprovado!')
+else:
+    print(nome, ',voce está aprovado!!')
